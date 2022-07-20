02ee3620444de19aa4f0931b589c2e4280eeda2f
Aakash Nadupalli <aakash.9372@gmail.com>
2022-07-20 19:01:54 +0530

    Add files via upload

--- Pycharm_Workspace/Data_Structures/Doubly_Linked_List.py
+++ Pycharm_Workspace/Data_Structures/Doubly_Linked_List.py
@@ -1,14 +1,13 @@
-# ## Linked List Implementation   Function Created Are Full ADT
-# import self as self
+### Doubly Linked List Implementation
 
 class Node:
-    def __init__(self, ele):
-        self.data = ele
+    def __init__(self, value):
+        self.data = value
         self.next = None
+        self.prev = None
 
 
-class Linked_List:
-
+class Db_Linked_List:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -16,27 +15,32 @@ class Linked_List:
 
     def insert_first(self, value):
         new_node = Node(value)
+
         self.size += 1
 
         if self.head is None:
             self.head = new_node
             self.tail = new_node
+
+            self.head.prev = None
             self.tail.next = None
 
         else:
             new_node.next = self.head
+            self.head.prev = new_node
             self.head = new_node
+            new_node.prev = None
 
-        print(f"After Adding First Element {self.head.data}")
-        self.print_list()
-
-    def insert_last(self, value):
+    def insert_Last(self, value):
         new_node = Node(value)
+
         self.size += 1
 
         if self.head is None:
             self.head = new_node
             self.tail = new_node
+
+            self.head.prev = None
             self.tail.next = None
 
         else:
@@ -44,37 +48,39 @@ class Linked_List:
             while temp.next:
                 temp = temp.next
             temp.next = new_node
+            new_node.prev = temp
             self.tail = new_node
             self.tail.next = None
 
-        print(f"After Adding Last Element {self.tail.data}")
-        self.print_list()
-
     def insert_After(self, after_which, value):
+
         new_node = Node(value)
         flag = False
 
         if self.head is None:
-            print("Element Not Present In the List.....")
+            print("Element Not Present In the List..... so cannot add value")
 
         elif self.head.data == after_which:
+            new_node.prev = self.head
             new_node.next = self.head.next
             self.head.next = new_node
+            new_node.next.prev = new_node
             flag = True
 
         elif self.tail.data == after_which:
-            self.tail.next = new_node;
-            self.tail = new_node
-            new_node.next = None
+            self.insert_Last(value)
             flag = True
 
         else:
             temp = self.head
             while temp:
                 if temp.data == after_which:
-                    new_node.next = temp.next
-                    temp.next = new_node
                     flag = True
+                    new_node.next = temp.next
+                    new_node.prev = temp
+                    temp.next = new_node
+                    new_node.next.prev = new_node
+                    break
                 temp = temp.next
 
         if not flag:
@@ -86,28 +92,44 @@ class Linked_List:
             print(f"Value {value} Inserted After {after_which}")
             self.print_list()
 
-            #   Below insert is just optional no need to create i just created for testing purpose
-
-    def insert(self, value):
+    def insert_Before(self, before_which, value):
         new_node = Node(value)
-        self.size += 1
+        flag = False
 
         if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-            self.tail.next = None
-            return
+            print("Element Not Present In the List..... so cannot add value")
 
-        if self.tail.next is None:
-            self.tail.next = new_node
-            self.tail = new_node
-            new_node.next = None
+        elif self.head.data == before_which:
+            self.insert_first(value)
+            flag = True
+
+        elif self.tail.data == before_which:
+            new_node.next = self.tail
+            new_node.prev = self.tail.prev
+            self.tail.prev = new_node
+            new_node.prev.next = new_node
+            flag = True
 
         else:
             temp = self.head
-            while (temp.next):
+            while temp:
+                if temp.data == before_which and temp.next is not None:
+                    flag = True
+                    new_node.next = temp
+                    new_node.prev = temp.prev
+                    temp.prev = new_node
+                    new_node.prev.next = new_node
+                    break
                 temp = temp.next
-            temp.next = new_node
+
+        if not flag:
+            print(
+                f"!!...  .   Element {before_which} Not Present In the List ...>> ---   Specify only the element which is in the List ",
+                "\n")
+        else:
+            self.size += 1
+            print(f"Value {value} Inserted Before {before_which}")
+            self.print_list()
 
     def delete_first(self):
         if self.head is None:
@@ -123,6 +145,7 @@ class Linked_List:
 
         print(f"After Deleting First Element {self.head.data}")
         self.head = self.head.next
+        self.head.prev = None
 
         print(end='')
         self.print_list()
@@ -147,6 +170,8 @@ class Linked_List:
             temp = temp.next
         temp.next = None
         self.tail = temp
+        self.tail.prev = temp.prev
+        self.tail.prev.next = temp
 
         print(end='')
         self.print_list()
@@ -169,6 +194,7 @@ class Linked_List:
             while temp.next is not None:
                 if temp.next.data == value:
                     flag = True
+                    temp.next.next.prev = temp
                     temp.next = temp.next.next
                     break
                 # print(temp.data,end=' ')
@@ -185,51 +211,53 @@ class Linked_List:
                 self.print_list()
 
 
+
+
     def print_list(self):
         if self.head is None:
             print("No Elements To Print...")
             return
 
-        print("Linked List Elements : ", end='')
+        print("Doubly Linked List Elements : ", end='')
         temp = self.head
+        print("None", end=" <--> ")
         while temp:
-            print(temp.data, end=" --> ")
+            print(temp.data, end=" <--> ")
             temp = temp.next
         print("None")
 
         print("Head = ", self.head.data, "  ", "Tail = ", self.tail.data, "  ", "Size = ", self.size, '\n')
 
-    ## Below one is using extra space    so below one is optional
-    def print_reverse(self):
+    def print_reverse_list(self):
         if self.head is None:
             print("No Elements To Print...")
             return
-        print("Reversed Linked List Elements  (No Change to Original Data) : ",end=' ')
-        list_elements = []
-        temp = self.head
-        while temp:
-            list_elements.append(temp.data)
-            temp = temp.next
-        for i in list_elements[::-1]:
-            print(i,end=' --> ')
+
+        print("Reverse Doubly Linked List Elements : ", end='')
+        temp = self.tail
+        print("None", end=" <--> ")
+        while temp is not None:
+            print(temp.data, end=" <--> ")
+            temp = temp.prev
+        print("None")
+
+        print("Head = ", self.tail.data, "  ", "Tail = ", self.head.data, "  ", "Size = ", self.size, '\n')
 
 
-
-
-    ### Initializing
-
-
-ll = Linked_List()
-roll_no = [f"21031F00{i:02d}" for i in range(2,30)]
-# elements = [2, 7, 9, 3, 5, 8, 1, 6, 25, 38, 78, 99, 11, 15, 65, 88]
-for i in roll_no:
-    ll.insert(i)
-ll.print_list()
-ll.insert_first("21031F0001")
-ll.insert_last("21031F0030")
-ll.delete("21031F0005")
-ll.delete("21031F0026")
-ll.insert_After("21031F0025","21031F0026")
-ll.delete_first()
-ll.delete_last()
-ll.print_reverse()
+Dl = Db_Linked_List()
+# roll_no = [f"21031F00{i:02d}" for i in range(1,31)]
+elements = [2, 7, 9, 3, 5, 8, 1, 6, 25, 38, 78, 99, 11, 15, 65, 88]
+for i in elements:
+    Dl.insert_Last(i)
+Dl.print_list()
+Dl.delete(3)
+Dl.delete(78)
+Dl.insert_first(10)
+Dl.insert_After(15, 20)
+Dl.insert_After(2, 300)
+Dl.print_reverse_list()
+Dl.insert_Before(2, 1)
+Dl.insert_Before(88, 50)
+Dl.insert_Before(99, 84)
+Dl.print_reverse_list()
+Dl.delete_last()
